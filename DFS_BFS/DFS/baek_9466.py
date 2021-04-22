b2673a16110caca68c9759ca31825d58d6db5cf5
kkw2758 <dtc3229@gmail.com>
2021-04-22 23:46:38 +0900

    21.04.22 DFS

--- DFS_BFS/DFS/baek_9466.py
+++ DFS_BFS/DFS/baek_9466.py
@@ -1,209 +1,5 @@
 '''
-import sys
-sys.setrecursionlimit(100000)
-
-def dfs(start, end):
-    stack = []
-    stack.append(graph[start - 1])
-    cnt = 1
-
-    while stack:
-        now, destination = stack.pop()
- 
-        visited[now - 1] = True
-
-        if destination == end:
-            return cnt
-
-        if not(visited[destination - 1]):
-            stack.append((destination, graph[destination - 1][1]))
-            cnt += 1
-    return 0
-
-t = int(input())
-for _ in range(t):
-    n = int(input())
-    choice_number = list(map(int,input().split()))
-    graph = []
-    for idx in range(len(choice_number)):
-        graph.append((idx + 1, choice_number[idx]))
-    
-    visited = [False] * n
-    result = n
-    for x in range(1, n + 1):
-        #visited = [False] * n
-        #print("-",x,dfs(x,x))
-        result -= dfs(x,x)
-        #if dfs(x,x):
-        #    result -= 1
-            #print("-",x, result)
-
-    print(result)
-'''
-'''
-import sys
-sys.setrecursionlimit(100000)
-
-def dfs(start, end):
-    stack = []
-    stack.append(graph[start - 1])
-    
-    while stack:
-        now, destination = stack.pop()
-        visited[now - 1] = True
-
-        if destination == end:
-            return True
-
-        if not(visited[destination - 1]):
-            stack.append((destination, graph[destination - 1][1]))
-
-    return False
-
-t = int(input())
-for _ in range(t):
-    n = int(input())
-    choice_number = list(map(int,input().split()))
-    graph = []
-    for idx in range(len(choice_number)):
-        graph.append((idx + 1, choice_number[idx]))
-
-
-    result_list = []
-    for x in range(1, n + 1):
-        if x not in result_list:
-            visited = [False] * n
-            if dfs(x,x):
-                for y in range(len(visited)):
-                    if visited[y]:
-                        result_list.append(y + 1)
-                        
-    print(n - len(result_list))
-    '''
-'''
-
-
-
-import sys
-sys.setrecursionlimit(100000)
-
-def dfs(start, end):
-    stack = []
-    stack.append((start, choice_number[start - 1]))
-    
-    while stack:
-        now, destination = stack.pop()
-        visited[now - 1] = True
-
-        dp[now][destination] = 1
-        
-        if destination == end:
-            return True
-
-        if not(visited[destination - 1]):
-            stack.append((destination, choice_number[destination - 1]))
-            dp[now][choice_number[destination -1]] = 1
-    return False
-
-t = int(input())
-for _ in range(t):
-
-    
-    
-    n = int(input())
-    dp = [[0] * n] * n
-    choice_number = list(map(int,input().split()))
-
-    result_list = []
-    for x in range(1, n + 1):
-        if x not in result_list:
-            visited = [False] * n
-            if dfs(x,x):
-                for y in range(len(visited)):
-                    if visited[y]:
-                        result_list.append(y + 1)
-                        
-    print(n - len(result_list))
-
-
-'''
-'''
-import sys
-sys.setrecursionlimit(111111) #충분한 재귀 깊이를 주어 오류를 예방
-
-
-def dfs(x):
-    global result
-    visited[x] = True
-    cycle.append(x) #사이클을 이루는 팀을 확인하기 위함
-    number = numbers[x]
-    
-    if visited[number]: #방문가능한 곳이 끝났는지
-        if number in cycle: #사이클 가능 여부
-            result += cycle[cycle.index(number):] #사이클 되는 구간 부터만 팀을 이룸
-            #print(result)
-        return
-    else:
-        dfs(number)
-
-
-for _ in range(int(input())):
-    N = int(input())
-    numbers = [0] + list(map(int, input().split()))
-    visited = [True] + [False] * N #방문 여부
-    result = []
-    
-    for i in range(1, N+1):
-        if not visited[i]: #방문 안한 곳이라면
-            cycle = []
-            dfs(i) #DFS 함수 돌림
-            print(i,result)
-            
-    print(N - len(result)) #팀에 없는 사람 수
-'''
-
-
-'''
-import sys
-sys.setrecursionlimit(100000)
-
-def dfs(start, end):
-    stack = []
-    stack.append(graph[start - 1])
-    cycle = []
-
-    while stack:
-        now, destination = stack.pop()
-        visited[now - 1] = True
-        cycle.append(now)
-
-        if destination == end:
-            return True
-
-        if not(visited[destination - 1]):
-            stack.append((destination, graph[destination - 1][1]))
-    return False
-
-t = int(input())
-for _ in range(t):
-    n = int(input())
-    choice_number = list(map(int,input().split()))
-    graph = []
-    for idx in range(len(choice_number)):
-        graph.append((idx + 1, choice_number[idx]))
-    
-    visited = [False] * n
-    result = n
-    for x in range(1, n + 1):
-        #visited = [False] * n
-        #print("-",x,dfs(x,x))
-        result -= dfs(x,x)
-        #if dfs(x,x):
-        #    result -= 1
-            #print("-",x, result)
-
-    print(result)
-'''
+#스택을 이용해서 구현을 했는데 메모리는 절약되는반면 시간이 오래걸림..
 import sys
 sys.setrecursionlimit(100000)
 
@@ -214,7 +10,7 @@ def dfs(start_node):
 
     while stack:
         now, destination = stack.pop()
-        visited[now -1] = True
+        visited[now - 1] = True
         cycle.append(now)
 
         if not(visited[destination - 1]):
@@ -242,4 +38,120 @@ for _ in range(t):
                 result += tmp
 
     print(n - len(result))
-            
+'''
+
+
+'''
+#스택을 이용한 빠른 풀이
+#시작 정점에서 방문할수있는 모든 정점을 방문하거나 사이클이 되는곳까지만 방문
+import sys
+sys.setrecursionlimit(100000)
+
+def dfs_stack(start_node):
+    stack = [start_node]
+    global result
+    while stack:
+        now_node = stack.pop()
+        # 방문처리 및 사이클처리
+        visited[now_node] = 1
+        cycle.append(now_node)
+        next_node = matrix[now_node]
+        
+        if not visited[next_node]:
+            stack.append(next_node)
+        else:
+            if next_node in cycle:
+                result += cycle[cycle.index(next_node):] 
+
+
+t = int(sys.stdin.readline())
+for _ in range(t):
+    n = int(sys.stdin.readline())
+
+    matrix = [0] + list(map(int, sys.stdin.readline().split()))
+    visited = [0] + [0] * n
+
+
+    result = []
+    for i in range(1, n + 1):
+        if not visited[i]:
+            cycle = []
+            dfs_stack(i)
+
+    print(n - len(result))
+'''
+
+
+'''
+#시작 정점에서 갈 수 있는 모든 정점을 탐색하기도전에 사이클여부를 확인하고있으므로 불필요 연산을 많이 하게됨.
+import sys
+sys.setrecursionlimit(1000000)
+
+def dfs_recursive(start_vertex):
+    result = []
+    if start_vertex in cycle:
+        return cycle[cycle.index(start_vertex):]
+
+    if not visited[start_vertex]:
+        visited[start_vertex] = 1
+        cycle.append(start_vertex)
+        result += dfs_recursive(matrix[start_vertex])
+
+    return result
+
+t = int(sys.stdin.readline())
+
+
+for _ in range(t):
+    n = int(sys.stdin.readline())
+
+    matrix = [0] + list(map(int, sys.stdin.readline().split()))
+    visited = [0] + [0] * n
+
+    result = 0
+    #cycle = []
+    for i in range(1, n + 1):
+        if not visited[i]:
+            cycle = []
+            result += len(dfs_recursive(i))
+
+
+    print(n - result)
+'''
+
+
+'''
+import sys
+sys.setrecursionlimit(1000000)
+
+def dfs_recursive(start_vertex):
+    global result
+    visited[start_vertex] = 1
+    cycle.append(start_vertex)
+
+    if not visited[matrix[start_vertex]]:
+        dfs_recursive(matrix[start_vertex])
+    else:
+        if matrix[start_vertex] in cycle:
+            result += cycle[cycle.index(matrix[start_vertex]):]
+            return
+
+
+
+t = int(sys.stdin.readline())
+for _ in range(t):
+    n = int(sys.stdin.readline())
+
+    matrix = [0] + list(map(int, sys.stdin.readline().split()))
+    visited = [0] + [0] * n
+
+
+    result = []
+    for i in range(1, n + 1):
+        if not visited[i]:
+            cycle = []
+            dfs_recursive(i)
+
+
+    print(n - len(result))
+'''
