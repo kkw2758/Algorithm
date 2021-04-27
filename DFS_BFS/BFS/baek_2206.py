a5ba83064a0e222310706f811f1fb5504a916af8
kkw2758 <dtc3229@gmail.com>
2021-04-27 23:21:48 +0900

    21.04.27 BFS sutdy

---
+++ DFS_BFS/BFS/baek_2206.py
@@ -0,0 +1,92 @@
+# 참고링크 : https://pacific-ocean.tistory.com/348
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+# visit[x][y][w] 라고할때 w 값이 1이면 벽을 안깬상태, w 값이 0이면 이미 벽을 깬 상태
+def bfs():
+    queue = deque()
+    queue.append([0, 0, 1])
+    visit = [[[0]*2 for i in range(m)] for j in range(n)]
+    visit[0][0][1] = 1 # 시작지점 방문처리
+
+    while queue:
+        x, y, w = queue.popleft()
+        if x == n - 1 and y == m - 1:
+            return visit[x][y][w]
+
+        for idx in range(4):
+            nx = x + dx[idx]
+            ny = y + dy[idx]
+            if 0 <= nx < n and 0 <= ny < m:
+                # 만약 다음 방문지점이 벽이고 아직 벽을 깨지 않았다면
+                # 벽을 꺠는 과정에서 다음지점의 방문 여부를 검사하지 않는 이유 - 벽을깨는순간 w = 0으로 전환되므로 똑같은곳에서 여러번 벽을 깰 필요가 없다.
+                if board[nx][ny] == 1 and w == 1:
+                    visit[nx][ny][0] = visit[x][y][w] + 1
+                    queue.append([nx,ny,0])
+                # 만약 다음 방문지점이 벽이아니고 아직 방문하지 않았다면
+                # visit[nx][ny][w] == 0 을 검사하는이유
+                # 만약 이미 방문했더라면 이미 이전에 방문했다는 뜻 즉, 더빠른 경로가 존재한다!
+                elif board[nx][ny] == 0 and visit[nx][ny][w] == 0:
+                    visit[nx][ny][w] = visit[x][y][w] + 1
+                    queue.append([nx, ny, w])
+    
+    return -1
+
+
+n, m = map(int, input().split())
+board = [list(map(int, list(input().strip()))) for row in range(n)]
+print(bfs())
+
+'''
+# 나의 오답 코드
+# 벽을 깬여부를 추가한것 까진 좋았지만 방문처리를 제대로 해줄 방법을 생각하지 못했다..
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs():
+    queue = deque()
+    queue.append((0,0,0))
+    level = 1
+    board[0][0] = 2
+    while queue:
+        queue_size = len(queue)
+        for _ in range(queue_size):
+            x, y, flag = queue.popleft()
+            for idx in range(4):
+                nx = x + dx[idx]
+                ny = y + dy[idx]
+                if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                    continue
+                if nx == n - 1 and ny == m - 1:
+                    return level + 1
+                #만약 벽이라면 board[nx][ny] == 1
+                if board[nx][ny] == 1:
+                    if flag == 0:   # 벽을 파괴할 수 있다면
+                        board[nx][ny] = 2
+                        queue.append((nx, ny, 1))
+                #만약 벽이아니라면 board[nx][ny] == 0
+                elif board[nx][ny] == 0:
+                    board[nx][ny] = 2
+                    queue.append((nx, ny, flag))
+
+        level += 1
+    
+    return -1
+
+                
+n, m = map(int, input().split())
+board = [list(map(int, list(input().strip()))) for row in range(n)]
+
+print(bfs())
+'''
--- DFS_BFS/BFS/baek_5427.py
+++ DFS_BFS/BFS/baek_5427.py
@@ -1,3 +1,4 @@
+'''
 import sys
 from collections import deque
 
@@ -7,10 +8,32 @@ dx = [1, -1, 0, 0]
 dy = [0, 0, -1, 1]
 
 
-def bfs(x, y):
+def fire_bfs(fire_list):
+    queue = deque(fire_list)
+    level = 1
+    while queue:
+        q_size = len(queue)
+        for _ in range(q_size):
+            x, y = queue.popleft()
+
+            for idx in range(4):
+                nx = x + dx[idx]
+                ny = y + dy[idx]
+                if 0 <= nx < h and 0 <= ny < w:
+                    if board[nx][ny] == ".":
+                        board[nx][ny] = level
+                        queue.append((nx, ny))
+    
+
+
+        level += 1
+
+def start_bfs(start_x, start_y):
+    if start_x == 0 or start_x == h - 1 or start_y == 0 or start_y == w -1:
+        return 1
     queue = deque()
-    queue.append((x, y))
-    board[x][y] = 0
+    queue.append((start_x, start_y))
+    board[start_x][start_y] = 0
 
     while queue:
         x, y = queue.popleft()
@@ -19,17 +42,16 @@ def bfs(x, y):
             ny = y + dy[idx]
             # nx와 ny의 범위가 맞고
             if 0 <= nx < h and 0 <= ny < w:
-                if board[nx][ny] == ".":
+                if board[nx][ny] == "#" or board[nx][ny] == "*":
+                    continue
+
+                if board[nx][ny] == "." or (board[x][y] + 1 < board[nx][ny]):
                     board[nx][ny] = board[x][y] + 1
+                    if nx == 0 or nx == h - 1 or ny == 0 or ny == w - 1:
+                        return board[nx][ny] + 1
                     queue.append((nx,ny))
-                
-                '''
-                아래주석 부분 수정필요
-                '''
-                #elif str(type(board[nx][ny])) == "<class 'int'>":
-                    # print("핳")
-                    # board[nx][ny] = min(board[nx][ny], board[x][y] + 1)
-                    # queue.append((nx,ny))
+
+    return "IMPOSSIBLE"
 
 
 t = int(input())
@@ -41,14 +63,84 @@ for _ in range(t):
 
     for row in range(h):
         tmp = list(input().strip())
-        board.append(tmp)
+        
+        if "@" in tmp:
+            start_x, start_y = row, tmp.index("@")
+
         fire_col = [ i for i, value in enumerate(tmp) if value == "*"]
         for col in fire_col:
-            fire_list.append((row, col)) 
+            fire_list.append((row, col))
 
-    print(fire_list)
-    print(board)
-    for fire in fire_list:
-        print(fire)
-        bfs(fire[0], fire[1])
-        print(board)
+        board.append(tmp)
+    
+    fire_bfs(fire_list)
+
+    print(start_bfs(start_x, start_y))
+'''
+
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def escape(start_x, start_y, fire_list):
+    if start_x == 0 or start_x == h - 1 or start_y == 0 or start_y == w -1:
+        return 1
+    queue = deque()
+    queue.append((start_x, start_y))
+    fire_queue = deque(fire_list)
+    level = 1
+    while queue:
+
+        fire_queue_size = len(fire_queue)
+        for _ in range(fire_queue_size):
+            x, y = fire_queue.popleft()
+            for idx in range(4):
+                nx = x + dx[idx]
+                ny = y + dy[idx]
+                if 0 <= nx < h and 0 <= ny < w:
+                    if board[nx][ny] == '.':
+                        board[nx][ny] = '*'
+                        fire_queue.append((nx, ny))       
+
+        queue_size = len(queue)
+        for _ in range(queue_size):
+            x, y = queue.popleft()
+            for idx in range(4):
+                nx = x + dx[idx]
+                ny = y + dy[idx]
+                if 0 <= nx < h and 0 <= ny < w:
+                    if board[nx][ny] == '.':
+                        if nx == 0 or nx == h - 1 or ny == 0 or ny == w - 1:
+                            return level + 1
+                        board[nx][ny] = '@'
+                        queue.append((nx, ny))
+
+        level += 1
+
+    return "IMPOSSIBLE"
+
+t = int(input())
+for _ in range(t):
+    w, h = map(int, input().split())
+    
+    fire_list = []
+    board = []
+
+    for row in range(h):
+        tmp = list(input().strip())
+        
+        if "@" in tmp:
+            start_x, start_y = row, tmp.index("@")
+
+        fire_col = [ i for i, value in enumerate(tmp) if value == "*"]
+        for col in fire_col:
+            fire_list.append((row, col))
+
+        board.append(tmp)
+
+    print(escape(start_x, start_y, fire_list))
