65a85ff758b62ccb7be6f24b3b0d0f47d98c6adc
kkw2758 <41050653+kkw2758@users.noreply.github.com>
2022-08-03 10:50:37 +0900

    Q11, Q12 Review

--- 구현/Q11.py
+++ 구현/Q11.py
@@ -1,68 +1,68 @@
-n = int(input())
-board = []
+# n = int(input())
+# board = []
 
-for _ in range(n + 2):
-    board.append([0] * (n + 2))
+# for _ in range(n + 2):
+#     board.append([0] * (n + 2))
 
-for i in range(len(board)):
-    for j in range(len(board[0])):
-        if (i == 0 or i == n + 1) or (j == 0 or j == n + 1):
-            board[i][j] = -1
-
-
-
-k = int(input())
-for i in range(k):
-    row, col = map(int, input().split())
-    board[row][col] = 1
-
-l = int(input())
-direction_list = []
-for i in range(l):
-    second, direction = input().split()
-    second = int(second)
-    direction_list.append((second, direction))
+# for i in range(len(board)):
+#     for j in range(len(board[0])):
+#         if (i == 0 or i == n + 1) or (j == 0 or j == n + 1):
+#             board[i][j] = -1
 
 
-# R D L U
-dx = [0, 1, 0 , -1]
-dy = [1, 0, -1, 0]
 
-def solution():
-    cnt = 0
-    x, y = 1, 1
-    board[x][y] = 2
-    direction = 0
-    body_list = [(1,1)]
-    while True:
-        if direction_list:
-            temp = direction_list[0]
-            if cnt == temp[0]:
-                if temp[1] == "L":
-                    direction = (direction - 1) % 4
-                elif temp[1] == "D":
-                    direction = (direction + 1) % 4
-                direction_list.pop(0)
-        #앞으로 가자
-        nx = x + dx[direction]
-        ny = y + dy[direction]
+# k = int(input())
+# for i in range(k):
+#     row, col = map(int, input().split())
+#     board[row][col] = 1
+
+# l = int(input())
+# direction_list = []
+# for i in range(l):
+#     second, direction = input().split()
+#     second = int(second)
+#     direction_list.append((second, direction))
+
+
+# # R D L U
+# dx = [0, 1, 0 , -1]
+# dy = [1, 0, -1, 0]
+
+# def solution():
+#     cnt = 0
+#     x, y = 1, 1
+#     board[x][y] = 2
+#     direction = 0
+#     body_list = [(1,1)]
+#     while True:
+#         if direction_list:
+#             temp = direction_list[0]
+#             if cnt == temp[0]:
+#                 if temp[1] == "L":
+#                     direction = (direction - 1) % 4
+#                 elif temp[1] == "D":
+#                     direction = (direction + 1) % 4
+#                 direction_list.pop(0)
+#         #앞으로 가자
+#         nx = x + dx[direction]
+#         ny = y + dy[direction]
         
-        if board[nx][ny] == -1 or board[nx][ny] == 2:
-            return cnt + 1
-        elif board[nx][ny] == 1: #사과 있으면!
-            body_list.append((nx, ny))
-            board[nx][ny] = 2
-        else:
-            body_list.append((nx, ny))
-            board[nx][ny] = 2
-            if body_list:
-                tx, ty = body_list.pop(0)
-                board[tx][ty] = 0
+#         if board[nx][ny] == -1 or board[nx][ny] == 2:
+#             return cnt + 1
+#         elif board[nx][ny] == 1: #사과 있으면!
+#             body_list.append((nx, ny))
+#             board[nx][ny] = 2
+#         else:
+#             body_list.append((nx, ny))
+#             board[nx][ny] = 2
+#             if body_list:
+#                 tx, ty = body_list.pop(0)
+#                 board[tx][ty] = 0
 
-        cnt += 1
-        x, y = nx, ny
+#         cnt += 1
+#         x, y = nx, ny
 
-print(solution())
+# print(solution())
 
 '''
 ex1)
@@ -103,3 +103,71 @@ ex3)
 11 D
 13 L
 '''
+
+n = int(input())
+arr = [[0] * n for _ in range(n)]
+
+k = int(input())
+
+for _ in range(k):
+    x, y = map(int, input().split())
+    arr[x - 1][y - 1] = 2
+
+l = int(input())
+direction_information = []
+for _ in range(l):
+    x, d = input().split()
+    direction_information.append((int(x), d))
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+
+snake_list = [(0, 0)]
+arr[0][0] = 1
+direction_idx = 1
+cnt = 0
+
+while True:
+    x, y = snake_list[-1]
+    # if direction_information:
+    #     if cnt == direction_information[0][0]:
+    #         direction = direction_information.pop(0)[1]
+    #         if direction == "D":
+    #             direction_idx = (direction_idx + 1) % 4
+    #         else:
+    #             direction_idx = (direction_idx - 1) % 4
+
+    # print("now_direction = {}".format(direction_idx))
+    nx, ny = x + dx[direction_idx], y + dy[direction_idx]
+    cnt += 1
+    if not(0 <= nx < n and 0 <= ny < n):    # 범위안에 안들어오면
+        break
+    else:
+        if arr[nx][ny] == 2:    # 사과면
+            snake_list.append((nx, ny))
+            arr[nx][ny] = 1
+        elif arr[nx][ny] == 1:   # 뱀 몸뚱아리면
+            break
+        else:   # 사과도 아니고 몸뚱아리도 아니면
+            snake_list.append((nx, ny))
+            arr[nx][ny] = 1
+            pop_x, pop_y = snake_list.pop(0)
+            arr[pop_x][pop_y] = 0
+    
+    if direction_information:
+        if cnt == direction_information[0][0]:
+            direction = direction_information.pop(0)[1]
+            if direction == "D":
+                direction_idx = (direction_idx + 1) % 4
+            else:
+                direction_idx = (direction_idx - 1) % 4
+
+    print("now_direction = {}".format(direction_idx))
+
+    print(cnt)
+    for _ in arr:
+        print(_)
+    print("=" * 50)
+
+print("result = {}".format(cnt))
--- 구현/Q12.py
+++ 구현/Q12.py
@@ -1,36 +1,72 @@
-def is_possible(answer):
-    for x, y, a in answer:
-        if a == 0:  # 기둥
-            if y == 0 or [x - 1, y, 1] in answer or [x, y, 1] in answer or [x, y - 1, 0] in answer:
-                continue
-            return False
-        elif a == 1:    # 보
-            if [x, y - 1, 0] in answer or [x + 1, y - 1, 0] in answer or ([x - 1, y, 1] in answer and [x + 1, y, 1] in answer):
-                continue
-            return False
+# def is_possible(answer):
+#     for x, y, a in answer:
+#         if a == 0:  # 기둥
+#             if y == 0 or [x - 1, y, 1] in answer or [x, y, 1] in answer or [x, y - 1, 0] in answer:
+#                 continue
+#             return False
+#         elif a == 1:    # 보
+#             if [x, y - 1, 0] in answer or [x + 1, y - 1, 0] in answer or ([x - 1, y, 1] in answer and [x + 1, y, 1] in answer):
+#                 continue
+#             return False
+
+#     return True
+
+
+# def solution(n, build_frame):
+#     answer = []
+#     for frame in build_frame:
+#         x, y, a, b = frame
+#         if b == 0:      # 삭제
+#             answer.remove([x, y, a])
+#             if not is_possible(answer):
+#                 answer.append([x, y, a])
+#         elif b == 1:    # 추가
+#             answer.append([x, y, a])
+#             if not is_possible(answer):
+#                 answer.remove([x, y, a])
+
+#     answer = sorted(answer)
+#     return answer
+
+
+
+# n = int(input())
+# build_frame = [[1,0,0,1],[1,1,1,1],[2,1,0,1],[2,2,1,1],[5,0,0,1],[5,1,0,1],[4,2,1,1],[3,2,1,1]]
+
+# print(solution(n, build_frame))
+
+def is_possible(frames):
+    for frame in frames:
+        x, y, a = frame
+        if a == 0: # 기둥일때
+            if not(y == 0 or [x - 1, y , 1] in frames or [x, y, 1] in frames or [x, y - 1, 0] in frames):
+                return False
+        elif a == 1:
+            if not([x, y - 1, 0] in frames or [x + 1, y - 1, 0] in frames or ([x - 1, y, 1] in frames and [x + 1, y, 1] in frames)):
+                return False
 
     return True
-
 
 def solution(n, build_frame):
     answer = []
     for frame in build_frame:
         x, y, a, b = frame
-        if b == 0:      # 삭제
-            answer.remove([x, y, a])
-            if not is_possible(answer):
-                answer.append([x, y, a])
-        elif b == 1:    # 추가
+        if b == 1:
             answer.append([x, y, a])
-            if not is_possible(answer):
+            if not(is_possible(answer)):
                 answer.remove([x, y, a])
+        elif b == 0:
+            print(frame)
+            answer.remove([x, y, a])
+            if not(is_possible(answer)):
+                answer.append([x, y, a])
 
-    answer = sorted(answer)
+
+    answer.sort()
     return answer
-
-
 
 n = int(input())
 build_frame = [[1,0,0,1],[1,1,1,1],[2,1,0,1],[2,2,1,1],[5,0,0,1],[5,1,0,1],[4,2,1,1],[3,2,1,1]]
+# build_frame = [[0,0,0,1], [2,0,0,1], [4,0,0,1], [0,1,1,1], [1,1,1,1], [2,1,1,1], [3,1,1,1], [2,0,0,0], [1,1,1,0], [2,2,0,1]]
 
 print(solution(n, build_frame))
