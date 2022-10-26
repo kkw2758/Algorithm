1aa8f60d61bfc94e36f78e260888d1a56421b8a6
kkw2758 <41050653+kkw2758@users.noreply.github.com>
2022-10-26 23:27:12 +0900

    2022/10/26 study

---
+++ etc/baek_1956.py
@@ -0,0 +1,27 @@
+# 2022/10/26 Baek 1956
+
+import sys
+input = sys.stdin.readline
+
+INF = int(1e9)
+
+v, e = map(int, input().split())
+graph = [[INF] * (v + 1) for _ in range(v + 1)]
+for _ in range(e):
+  a, b, c = map(int, input().split())
+  graph[a][b] = c
+
+for k in range(1, v + 1):
+  for i in range(1, v + 1):
+    for j in range(1, v + 1):
+      graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+result = INF
+
+for i in range(1, v + 1):
+  result = min(result, graph[i][i])
+
+if result == INF:
+  print(-1)
+else:
+  print(result)
