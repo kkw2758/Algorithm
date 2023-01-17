ab9a6533e315a7b80dc52a896ed9d2e0e69c94e0
kkw2758 <41050653+kkw2758@users.noreply.github.com>
2023-01-17 23:35:47 +0900

    2023/01/17 study

---
+++ graph/baek_9372.py
@@ -0,0 +1,35 @@
+# 2023/01/16 Baek 9372
+
+# 상근이의 여행
+# 가장 적은 종류의 비행기
+
+T = int(input())
+
+def dfs():
+  stack = []
+  # node_number, cnt
+  stack.append(1)
+  
+  visited = [-1] * (N + 1)
+  visited[1] = 0
+  result = 0
+  while stack:
+    node_number= stack.pop()
+    for next_node in Tree[node_number]:
+      if visited[next_node] == -1:
+        visited[next_node] = 0
+        stack.append(next_node)
+        result += 1
+
+  return result
+
+for _ in range(T):
+  N, M = map(int, input().split())
+  Tree = [[] for _ in range(N + 1)]
+  
+  for _ in range(M):
+    a, b = map(int, input().split())
+    Tree[a].append(b)
+    Tree[b].append(a)
+
+  print(dfs())
--- 구현/baek_1331.py
+++ 구현/baek_1331.py
@@ -1,23 +1,49 @@
-# 2023/01/16 Baek 1331
+# # 2023/01/16 Baek 1331
 
-board = [[False] * 6 for _ in range(6)]
+# board = [[False] * 6 for _ in range(6)]
+# indexes = []
+# for _ in range(36):
+#   index = input()
+#   x = ord(index[0]) - ord("A")
+#   y = int(index[1]) - 1
+#   indexes.append((x, y))
+
+# def check():
+#   for x, y in indexes:
+#     if not board[x][y]:
+#       board[x][y] = True
+#     else:
+#       return "Invalid"
+  
+#   return "Valid"
+
+# print(check())
+
+# for _ in board:
+#   print(_)
+
+
+d = [[-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2], [2, -1], [2, 1]]
+visited = [[0] * 6 for _ in range(6)]
 indexes = []
-for _ in range(36):
+for i in range(36):
   index = input()
   x = ord(index[0]) - ord("A")
   y = int(index[1]) - 1
   indexes.append((x, y))
 
-def check():
-  for x, y in indexes:
-    if not board[x][y]:
-      board[x][y] = True
-    else:
-      return "Invalid"
-  
-  return "Valid"
 
-print(check())
-
-for _ in board:
-  print(_)
+x, y = indexes[0][0], indexes[0][1]
+ex, ey = indexes[-1][0], indexes[-1][1]
+if [ex - x, ey - y] not in d:
+  print("Invalid")
+else:
+  visited[x][y] = 1
+  for nx, ny in indexes[1:]:
+    if [nx - x, ny - y] not in d or visited[nx][ny] != 0:
+      print("Invalid")
+      break
+    visited[nx][ny] += 1
+    x, y = nx, ny
+  else:
+    print("Valid")
