da99667b542e293b9c15b9a1b8eb9762b66f70b4
kkw2758 <41050653+kkw2758@users.noreply.github.com>
2023-02-08 01:23:02 +0900

    2023/02/07 study

---
+++ 구현/baek_11728.py
@@ -0,0 +1,12 @@
+# 2023/02/07 Baek 11728
+
+import sys
+input = sys.stdin.readline
+
+
+N, M = map(int, input().split())
+A = list(map(int, input().strip().split()))
+B = list(map(int, input().strip().split()))
+result = sorted(A+B)
+for i in result:
+  print(i, end = " ")
