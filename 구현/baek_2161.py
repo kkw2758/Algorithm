3cf4dca5d79a9275b8913feb3eed5bb3ee9730a1
kkw2758 <41050653+kkw2758@users.noreply.github.com>
2023-02-17 01:33:38 +0900

    2023/02/16 study

--- 구현/baek_2161.py
+++ 구현/baek_2161.py
@@ -2,9 +2,12 @@
 
 N = int(input())
 numbers = [i for i in range(1, N + 1)]
-for i in range(N):
-    print(numbers.pop(0), end = " ")
-    if len(numbers) == 1:
-      print(numbers[0], end= "")
-      break
-    numbers.append(numbers.pop(0))
+if N == 1:
+   print(1)
+else:
+  for i in range(N):
+      print(numbers.pop(0), end = " ")
+      if len(numbers) == 1:
+        print(numbers[0], end= "")
+        break
+      numbers.append(numbers.pop(0))
