ff0a82289ea8644933502fb08f7fef2f74f7c685
kkw2758 <41050653+kkw2758@users.noreply.github.com>
2023-02-17 23:18:04 +0900

    2023/02/17 study

---
+++ 정렬/baek_11931.py
@@ -0,0 +1,14 @@
+# 2023/02/17 Baek 11931
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+numbers = []
+for _ in range(N):
+  numbers.append(int(input()))
+
+numbers.sort(reverse=True)
+
+for number in numbers:
+  print(number)
